9d8288153fd611702897dde5c73d0387ce23aeba
Ammar Yazdani <ammaryazdani2@gmail.com>
2026-08-03 22:27:34 +0530

    Implement JWT authentication and protected user routes

--- backend/app/api/routes/auth.py
+++ backend/app/api/routes/auth.py
@@ -5,6 +5,7 @@ from app.auth.password import hash_password
 from app.database.database import get_db
 from app.models.user import User
 from app.schemas.user import UserCreate, UserResponse
+from auth.jwt import create_access_token
 
 router = APIRouter(
     prefix="/auth",
@@ -41,5 +42,38 @@ def register(
 
     return new_user
 
+
+@router.post("/login", response_model=Token)
+def login(
+    user: UserLogin,
+    db: Session = Depends(get_db)
+):
+    db_user = (
+        db.query(User)
+        .filter(User.email == user.email)
+        .first()
+    
+    )
+
+    if not db_user:
+        raise HTTPException(
+            status_code = 401,
+            detail = "Invalid email or password"
+
+        )
+    if not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(
+            status_code = 401,
+            detail = "Invalid email or Password"
+        )
+    acess_token = create_access_token(
+        data = {"sub": db_user.email}
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
+    
 
     
--- backend/app/auth/jwt.py
+++ backend/app/auth/jwt.py
@@ -2,7 +2,8 @@ from datetime import datetime, timedelta
 from jose import jwt
 from app.config.config import settings
 
-def create_access_token(data: dict):
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
     payload = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     payload["exp"] = expire
